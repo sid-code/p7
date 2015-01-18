e465fe74a9397bb3adf690db379950a183325c32
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-01-18 14:08:06 -0700

    Add option to Makefile to parallelize
    use "make PARALLEL=yes"

--- p7multi.c
+++ p7multi.c
@@ -25,11 +25,13 @@ int main() {
 #define THROWS 1000000000
 
 #define START 1.01
-#define END   1.10
+#define END   1.03
 #define STEP  0.01
 
   int num_threads = (END - START) / STEP;
+#ifdef PARALLEL
   pthread_t mc_threads[num_threads];
+#endif
   int index = 0;
 
   double seed;
@@ -42,13 +44,18 @@ int main() {
 
     pthread_t mc_thread;
     pthread_create(&mc_thread, NULL, monte_carlo_large, task);
+#ifndef PARALLEL
+    pthread_join(mc_thread, NULL);
+#else
     mc_threads[index] = mc_thread;
+#endif
   }
 
+#ifdef PARALLEL
   for (index = 0; index < num_threads; index++) {
     pthread_join(mc_threads[index], NULL);
   }
-
+#endif
 
   printf("Finished %d threads.\n", num_threads);
 
